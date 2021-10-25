fba7a28147fa3d15735420ca97a786e406f3ab24
AmnesiaBeing <727184139@qq.com>
2021-10-25 23:55:26 +0800

    使用css绘图制作时间轴。

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,33 +1,19 @@
 <template>
   <ul class="timeline">
-    <li>
-      <div class="caption">caption</div>
-      <div class="mark" />
-      <div class="datetime">datetime</div>
-    </li>
-    <li>
-      <div class="caption">caption</div>
-      <div class="mark" />
-      <div class="datetime">datetime</div>
-    </li>
-    <li>
-      <div class="caption">caption</div>
-      <div class="mark" />
-      <div class="datetime">datetime</div>
-    </li>
+    <timeline-item caption="abc" value="1" datetime="123"/>
+    <timeline-item caption="abc" value="1.25" datetime="123"/>
+    <timeline-item caption="abc" value="1" datetime="123"/>
   </ul>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
-import Axis from './Axis.vue';
-import Marker from './Marker.vue';
+import TimelineItem from './TimelineItem.vue';
 
 export default defineComponent({
   name: 'TimeLine',
   props: {},
-  components: {},
+  components: {TimelineItem},
   setup() {
     return {};
   },
@@ -37,34 +23,7 @@ export default defineComponent({
 <style scoped>
 .timeline {
   display: flex;
-}
-.timeline li {
-  display: inline-block;
-  flex: 1;
-}
-.timeline .caption {
-  width: 100%;
-  text-align: center;
-}
-.timeline .datetime {
-  width: 100%;
-  text-align: center;
-}
-.timeline .mark {
-  width: 0;
-  height: 0;
-  border-left: 5px solid transparent;
-  border-right: 5px solid transparent;
-  border-top: 10px solid red;
-}
-.timeline .mark::before {
-  height: 2px;
-  left: 0px;
-  background: black;
-}
-.timeline .mark::after {
-  height: 2px;
-  right: 0px;
-  background: black;
+  flex-wrap: wrap;
+  padding: 0;
 }
 </style>
---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,93 @@
+<template>
+  <li>
+    <div class="caption">{{ caption }}</div>
+    <div class="axis" />
+    <div class="mark" />
+    <div class="datetime">{{ datetime }}</div>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TimeLineItem',
+  props: {
+    caption: String,
+    datetime: String,
+    value: Number,
+  },
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.timeline {
+  display: flex;
+  padding: 0;
+}
+.timeline li {
+  display: inline-block;
+  flex: v-bind(value);
+  position: relative;
+}
+.timeline .caption {
+  width: 100%;
+  text-align: center;
+  margin-bottom: 8px;
+  font-size: 8px;
+}
+.timeline .datetime {
+  width: 100%;
+  text-align: center;
+  margin-top: -10px;
+  font-size: 8px;
+}
+.timeline .axis {
+  width: 100%;
+  height: 2px;
+  background: black;
+  position: relative;
+}
+.timeline .mark {
+  position: relative;
+  width: 0;
+  height: 0;
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
+  border-top: 8px solid black;
+  content: '';
+  left: 50%;
+  margin-left: -6px;
+  top: 0%;
+  transform: translateY(-9px);
+}
+.timeline .mark::after {
+  content: '';
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-left: 3px solid transparent;
+  border-right: 3px solid transparent;
+  border-top: 4px solid white;
+  content: '';
+  left: 50%;
+  margin-left: -3px;
+  top: 50%;
+  transform: translateY(-6.5px);
+}
+.timeline li:last-child .axis::after {
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-top: 6px solid transparent;
+  border-bottom: 6px solid transparent;
+  border-left: 6px solid black;
+  content: '';
+  right: -6px;
+  bottom: -5px;
+}
+</style>
